e8f5ac04430272c095673661091d0176445c6f4b
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-06 13:52:54 +0530

    Added The Computation Result in the Descending Order

--- arithmetic.sh
+++ arithmetic.sh
@@ -16,9 +16,11 @@ num4=$(($a%$b+$c));
 
 declare -A num
 
-num[1]=$(($a+$b*$c));
-num[2]=$(($a*$b+$c));
-num[3]=$(($c+$a/$b));
-num[4]=$(($a%$b+$c));
+num[0]=$(($a+$b*$c));
+num[1]=$(($a*$b+$c));
+num[2]=$(($c+$a/$b));
+num[3]=$(($a%$b+$c));
 
 echo "${num[@]}"
+
+sort -nr <(printf "%s\n" "${num[@]}")
