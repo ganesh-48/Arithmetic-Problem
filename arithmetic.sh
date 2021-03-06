f071cb2c36e79c22f6f0b50d747c2fdf1130f437
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-06 12:47:32 +0530

    Added Read The Values from the Dictionary into the Array

--- arithmetic.sh
+++ arithmetic.sh
@@ -7,9 +7,18 @@ read a
 read b
 read c
 
-declare -A num
-
 num1=$(($a+$b*$c));
 num2=$(($a*$b+$c));
 num3=$(($c+$a/$b));
 num4=$(($a%$b+$c));
+
+
+
+declare -A num
+
+num[1]=$(($a+$b*$c));
+num[2]=$(($a*$b+$c));
+num[3]=$(($c+$a/$b));
+num[4]=$(($a%$b+$c));
+
+echo "${num[@]}"
